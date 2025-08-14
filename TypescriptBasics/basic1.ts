6a6b24ff6cbfe63fa0cbf3e93b7317e369a3a8b8
Siddham Gaonkar <sidgaonkar@gmail.com>
2025-08-14 12:54:32 +0530

    typescript addition

--- TypescriptBasics/basic1.ts
+++ TypescriptBasics/basic1.ts
@@ -1,5 +1,6 @@
-let message : string = "hello";
 
+
+let message : string = "hello";
 let age : number =10;
 let isactive : boolean = true;
 let numArray : number[] =[1,2,3,4,5]
@@ -8,5 +9,20 @@ let data : any ="this will behave as javascript"
 data=42;
 
 console.log(message,age,isactive,numArray)
+
+// function declaration
+function add(a:number,b:number):number //--> this makes return type as number
+{
+    return a+b;
+}
+
+//declaring objects
+
+let users:{name:string,age:number} =
+{
+    name:"Bob",
+    age: 4
+}
+
 
 
