a915081961aed0a74fa03a35606d2c963c89652c
Siddham Gaonkar <sidgaonkar@gmail.com>
2025-08-14 12:13:57 +0530

    from laptop

---
+++ TypescriptBasics/basic1.ts
@@ -0,0 +1,12 @@
+let message : string = "hello";
+
+let age : number =10;
+let isactive : boolean = true;
+let numArray : number[] =[1,2,3,4,5]
+
+let data : any ="this will behave as javascript"
+data=42;
+
+console.log(message,age,isactive,numArray)
+
+
